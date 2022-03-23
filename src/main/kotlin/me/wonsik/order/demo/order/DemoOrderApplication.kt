c2835d7d6afdaabe935ef0b13f7e56377e804638
wonsik-cheung <cws0173@gmail.com>
2022-03-23 15:42:58 +0900

    init: project setting

---
+++ src/main/kotlin/me/wonsik/order/demo/order/DemoOrderApplication.kt
@@ -0,0 +1,11 @@
+package me.wonsik.order.demo.order
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoOrderApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoOrderApplication>(*args)
+}
---
+++ src/test/kotlin/me/wonsik/order/demo/order/DemoOrderApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.wonsik.order.demo.order
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoOrderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
