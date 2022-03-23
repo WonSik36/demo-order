530bcb94c7ef0121b1f64380406af29f16e5f05a
wonsik-cheung <cws0173@gmail.com>
2022-03-23 18:55:54 +0900

    feat: BaseEntity 작성

--- src/main/kotlin/me/wonsik/order/demo/order/DemoOrderApplication.kt
+++ src/main/kotlin/me/wonsik/order/demo/order/DemoOrderApplication.kt
@@ -2,7 +2,9 @@ package me.wonsik.order.demo.order
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
 @SpringBootApplication
 class DemoOrderApplication
 
