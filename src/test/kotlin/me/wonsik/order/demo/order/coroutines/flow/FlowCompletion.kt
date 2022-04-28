3754d9273ebcb4aa6d18d25915738cc434ca773e
wonsik-cheung <cws0173@gmail.com>
2022-04-29 08:55:37 +0900

    test: flow onCompletion

---
+++ src/test/kotlin/me/wonsik/order/demo/order/coroutines/flow/FlowCompletion.kt
@@ -0,0 +1,68 @@
+package me.wonsik.order.demo.order.coroutines.flow
+
+import io.kotest.core.spec.style.FreeSpec
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onCompletion
+
+
+/**
+ * @author 정원식 (wonsik.cheung)
+ */
+class FlowCompletion : FreeSpec({
+    "finally" - {
+        "with exception" {
+            try {
+                exFlow().collect { println(it) }
+            } finally {
+                println("finally - with exception done")
+            }
+        }
+
+        "without exception" {
+            try {
+                simpleFlow().collect { println(it) }
+            } finally {
+                println("finally - without exception done")
+            }
+        }
+    }
+
+    "onCompletion" - {
+        "with exception" {
+            exFlow()
+                .onCompletion { println("onCompletion - with exception done") }
+                .collect { println(it) }
+        }
+
+        "without exception" {
+            simpleFlow()
+                .onCompletion { println("onCompletion - with exception done") }
+                .collect { println(it) }
+        }
+
+        "cause" {
+            exFlow()
+                .onCompletion { cause ->
+                    if (cause == null) {
+                        println("exception was not occurred")
+                    } else {
+                        println("exception was occurred")
+                    }
+                }
+                .collect { println(it) }
+        }
+    }
+})
+
+fun simpleFlow() = flow {
+    for (i in 1..3) {
+        emit(i)
+    }
+}
+
+fun exFlow() = flow {
+    for (i in 1..3) {
+        emit(i)
+    }
+    throw IllegalStateException("exFlow: throw ex")
+}
