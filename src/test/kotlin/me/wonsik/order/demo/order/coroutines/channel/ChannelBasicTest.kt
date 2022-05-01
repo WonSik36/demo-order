9bf6a2e53dcc994b016ca2ed169d1233cf6cb422
wonsik-cheung <cws0173@gmail.com>
2022-05-02 08:17:18 +0900

    test: ProducerCoroutine

--- src/test/kotlin/me/wonsik/order/demo/order/coroutines/channel/ChannelBasicTest.kt
+++ src/test/kotlin/me/wonsik/order/demo/order/coroutines/channel/ChannelBasicTest.kt
@@ -1,7 +1,10 @@
 package me.wonsik.order.demo.order.coroutines.channel
 
 import io.kotest.core.spec.style.FreeSpec
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
@@ -13,6 +16,7 @@ import kotlinx.coroutines.launch
  * 참조
  * @see <a href="https://dalinaum.github.io/coroutines-example/17">채널 기초</a>
  */
+@ExperimentalCoroutinesApi
 class ChannelBasicTest : FreeSpec({
     "Basic" - {
         "send - receive" {
@@ -47,5 +51,35 @@ class ChannelBasicTest : FreeSpec({
             }
             println("완료")
         }
+    }
+
+    "close" {
+        val channel = Channel<Int>()
+
+        launch {
+            for (x in 1..10) {
+                channel.send(x)     // suspension point
+            }
+            channel.close()
+        }
+
+        for (value in channel) {
+            println(value)
+        }
+        println("완료")
+    }
+
+    "ProducerCoroutine" {
+        val channel = produce<Int> {      // ProducerCoroutine -> ProducerScope = CoroutineScope + SendChannel
+            for(x in 1..10) {       // ProducerCoroutine -> ChannelCoroutine (AbstractCoroutine, Channel)
+                channel.send(x)
+            }
+            channel.close()
+        }
+
+        channel.consumeEach {
+            println(it)
+        }
+        println("완료")
     }
 })
